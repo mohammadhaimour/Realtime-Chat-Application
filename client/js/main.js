c1fd5853050133029e1c9f6d4758cfb619aec415
Salehziad <salehziad1999@gmail.com>
2022-06-30 13:22:04 +0300

    v3

--- client/js/main.js
+++ client/js/main.js
@@ -1,27 +1,31 @@
 const chatMessages = document.querySelector('.chat-messages');
 const chatForm = document.getElementById('chat-form');
-const roomName=document.getElementById('room-name');
-const usersNames=document.getElementById('users')
+const roomName = document.getElementById('room-name');
+const usersNames = document.getElementById('users')
 // get username and room name from url 
-const{username,room}=Qs.parse(location.search,{
-    ignoreQueryPrefix:true
+const {
+    username,
+    room
+} = Qs.parse(location.search, {
+    ignoreQueryPrefix: true
 });
-console.log(username,room);
+console.log(username, room);
 const socket = io();
 // get room and users
-socket.on('user-rooms-info',({room,users})=>{
-    // console.log({room});
-    // let x=users.username
-    // console.log(x);
+socket.on('user-rooms-info', ({
+    room,
+    users
+}) => {
     outputRoom(room);
     outputUsers(users);
 })
 // chatroom joining whth room name and username
-socket.emit('joinRoom',{username,room});
+socket.emit('joinRoom', {
+    username,
+    room
+});
 // message from server
 socket.on('message', message => {
-    // console.log("ratacke from server and handle from index",message);
-    // console.log(message);
     outputMessage(message);
     //scroll every comming message
     chatMessages.scrollTop = chatMessages.scrollHeight;
@@ -30,20 +34,17 @@ socket.on('message', message => {
 // submit messsge
 chatForm.addEventListener('submit', (e) => {
     e.preventDefault();
-
     const message = e.target.message.value;
-    // console.log('sent from index',message);
     // emmit message to server
     socket.emit('chatMessage', message);
     //clear input after send
-    e.target.message.value='' // focus means empty
- // focus means empty
+    e.target.message.value = '' // focus means empty
+    // focus means empty
 });
 
+// functions
 // output message to client
 function outputMessage(message) {
-    // console.log("gggggggggggggggggggggggggggggggggggggggggggggggggggggggggg");
-    // console.log({message});
     const div = document.createElement('div');
     div.classList.add('message');
     div.innerHTML = `
@@ -53,13 +54,17 @@ function outputMessage(message) {
     </p>`;
     document.querySelector('.chat-messages').appendChild(div);
 }
+
 // output room name to client
-function outputRoom(room){
-    roomName.innerText=room;
+function outputRoom(room) {
+    roomName.innerText = room;
 }
+
 // output users to client
-function outputUsers(users){
-    console.log({users});
-    const dev=document.createElement('li');
-    usersNames.innerHTML=`${users.map(user=>`<li>${user.username} <span>•</span> </li>`).join('')}`
+function outputUsers(users) {
+    console.log({
+        users
+    });
+    const dev = document.createElement('li');
+    usersNames.innerHTML = `${users.map(user=>`<li> <span class="dot">•</span> ${user.username}</li>`).join('')}`
 }
--- index.js
+++ index.js
@@ -2,9 +2,15 @@
 require('dotenv').config();
 const express = require('express');
 const PORT = process.env.PORT;
+// The path module provides utilities for working with file and directory paths.
+
 const path = require('path');
+// create node.js server with core 'http' module ====== */
+
 const http = require('http');
 const socketio = require('socket.io');
+// require functions
+
 const formateMessage = require('./utils/message');
 const {
     join,
@@ -13,66 +19,62 @@ const {
     getRoom,
     getall
 } = require('./utils/users');
-// console.log({join});
-// set static folder
+
+// stert the app and connection with files 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 app.use(express.static(path.join(__dirname, 'client')));
+
 // run when client connect
 io.on('connection', socket => {
-    // console.log("doooooo");
     // handle join from client with name of room and username
     socket.on('joinRoom', ({
         username,
         room
     }) => {
-        // console.log(username, room);
+        // add user info and room to the data
         const user = join(socket.id, username, room);
-        socket.join(user.room)
+        socket.join(user.room);
+
         // send welcome message when join chat
-        console.log('allllllllllllllllll',getall());
         socket.emit('message', formateMessage('LTUC BOT', 'welcome to LTUC chat'));
-        //WHEN USER CONNECT
+
+        //WHEN USER CONNECT brodcast message to all users in the room
         socket.broadcast.to(user.room).emit('message', formateMessage('LTUC BOT', `${user.username} has joined chat`));
+
         // send users and room info
-        // let x=user.room;
-        // console.log('sssssssss',{x});
-        // let xx=getRoom(user.room);
-        // console.log({xx});
         io.to(user.room).emit('user-rooms-info', ({
             room: user.room,
             users: getRoom(user.room)
-        }))
+        }));
     });
+
+    // catch chatMessage from client
+    socket.on('chatMessage', message => {
+        // get the user who have this socket id
+        const user = getUser(socket.id);
+        // emit the user info to the room with information and message
+        io.to(user[0].room).emit('message', formateMessage(user[0].username, message));
+    });
+
     //handle when client leave chat
     socket.on('disconnect', () => {
-        // to know wich user leave
+        // to know wich user leave by socket id
         const user = userLeave(socket.id);
-        // console.log({
-        //     user
-        // });
         // check if there user
         if (user) {
-            //this send to all users
+            //this send to all users that user has leave
             io.to(user[0].room).emit('message', formateMessage('LTUC BOT', `${user[0].username} has left the chat`));
-            // send users and room info
+
+            // send users and room info to handle them in client
             io.to(user.room).emit('user-rooms-info', ({
                 room: user[0].room,
                 users: getRoom()
             }))
         }
     });
-    // catch chatMessage from client
-    socket.on('chatMessage', message => {
-        // console.log('sent from index and handle from server', message);
-        console.log(socket.id);
-        const user = getUser(socket.id);
-        // console.log('aaaaaaaaaaaaaaaaaaaaa',user);
-        let x=user[0].username;
-        // console.log({x});
-        io.to(user[0].room).emit('message', formateMessage(user[0].username, message));
-    });
+
 })
 // console.log(PORT);
 server.listen(PORT, () => {
